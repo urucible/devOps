28ae6f781d5b948af24f883625e16cc835e7736f
urucible <urucible@students.zhaw.ch>
2023-05-24 22:46:31 +0200

    show chart of students in class and their age

--- gradleProject/app/src/main/java/gradledevopsproject/App.java
+++ gradleProject/app/src/main/java/gradledevopsproject/App.java
@@ -3,6 +3,11 @@
  */
 package gradledevopsproject;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 import com.indvd00m.ascii.render.Render;
 import com.indvd00m.ascii.render.api.ICanvas;
 import com.indvd00m.ascii.render.api.IContextBuilder;
@@ -25,6 +30,23 @@ public class App {
         ICanvas canvas = render.render(builder.build());
         String s = canvas.getText();
         System.out.println(s);
+
+        // As project has been shown in ASCII code, here a chart plot using JFreeChart
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(20, "Student", "Patrick");
+        dataset.addValue(28, "Student", "Blendor");
+        dataset.addValue(21, "Student", "Michael");
+        dataset.addValue(23, "Student", "Veronique");
+        dataset.addValue(25, "Student", "Patricia");
+        dataset.addValue(19, "Student", "Maria");
+        
+        // Create a bar chart
+        JFreeChart chart = ChartFactory.createBarChart("Students in class", "Names", "Ages", dataset);
+        
+        // Display the chart in a window
+        ChartFrame frame = new ChartFrame("Students Chart", chart);
+        frame.pack();
+        frame.setVisible(true);
 
     }
 }
